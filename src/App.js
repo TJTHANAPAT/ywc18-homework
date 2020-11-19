eb0283a28d0471dea11f2e594b02a894867cfd70
tjthanapat <thanapat-ja@hotmail.com>
2020-11-19 22:58:16 +0700

    Add Category and Subcategory Filter and Style

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React from 'react';
 
-import './style.css';
+import './css/style.css';
+import './css/bootstrap.min.css'
 import './App.css';
 import MerchantDetail from './MerchantDetail'
 
 class App extends React.Component {
   state = {
-    isLoading: true,
+    // isLoading: true,
     jsonData: null,
+    filterKeyCategory: 'all',
   }
   componentDidMount = async () => {
     fetch('https://panjs.com/ywc18.json')
       .then(response => response.json())
       .then((jsonData) => {
         console.log(jsonData)
+        jsonData.categories.push({name: 'งานบริการอื่นๆ / เบ็ดเตล็ด', subcategories: []})
         this.setState({
           isLoading: false,
           jsonData: jsonData,
@@ -25,10 +28,126 @@ class App extends React.Component {
       })
   }
 
+  filterCategory = () => {
+    const { jsonData } = this.state;
+    let categoryOptions = () => {
+      if (!!jsonData) {
+        const { categories } = jsonData;
+        let options = categories.map((category, i) => {
+          return (
+            <div key={i}>
+              <input 
+                type="radio" name="filterKeyCategory" 
+                id={category.name} value={category.name} 
+                onClick={this.handleFilterCategory}
+              />
+              <label htmlFor={category.name} className="ml-1">{category.name}</label>
+            </div>
+          )
+        })
+        return options
+      }
+    }    
+    return (
+      <div className="filter" style={{marginBottom: '17px'}}>
+        <label className="filter-name">ประเภทร้านค้า</label>
+        <div>
+          <input 
+            type="radio" name="filterKeyCategory" id="allcategories" 
+            value="all" onClick={this.handleFilterCategory} defaultChecked
+          />
+          <label htmlFor="allcategories" className="ml-1">ทั้งหมด</label>
+        </div>
+        {categoryOptions()}
+      </div>
+    )
+  }
+
+  handleFilterChange = () => {
+    const { jsonData, filterKeyCategory, filterKeySubcatagory } = this.state
+    const { merchants } = jsonData
+    let merchantsFiltered = []
+    console.log(filterKeyCategory)
+    if (filterKeyCategory === 'all') {
+      merchantsFiltered = merchants
+    } else if (filterKeyCategory === 'ร้านอาหารและเครื่องดื่ม') {
+      merchantsFiltered = merchants.filter((item) => {
+        return (item.categoryName==='ร้านอาหารและเครื่องดื่ม' || item.categoryName==='ร้านอาหาร' || item.categoryName==='ร้านเครื่องดื่ม')
+      })
+    } else if (filterKeyCategory === 'สินค้าทั่วไป') {
+      merchantsFiltered = merchants.filter((item) => {
+        return (item.categoryName==='สินค้าทั่วไป' || item.categoryName==='แฟชั่น')
+      })
+    } else {
+      merchantsFiltered = merchants.filter((item) => {
+        return (item.categoryName===filterKeyCategory)
+      })
+    }
+    console.log('filtered merchants: ', merchantsFiltered)
+    this.setState({merchantsFiltered: merchantsFiltered})
+  }
+
+  handleFilterCategory = (event) => {
+    console.log(event.target.value)
+    this.setState({
+      filterKeyCategory: event.target.value,
+      filterKeySubcatagory: 'all'
+    }, ()=>{
+      console.log(this.state.filterKeyCategory)
+      this.handleFilterChange()
+    });
+  }
+
+  filterSubcategory = () => {
+    const { filterKeyCategory } = this.state;
+    if (filterKeyCategory === 'all') {
+      return 
+    } else {
+      const { jsonData } = this.state;
+      const { categories } = jsonData;
+      let subcategories = []
+      for (var i = 0; i < categories.length; i++) {
+        if (categories[i].name === filterKeyCategory) {
+          subcategories = categories[i].subcategories
+        }
+      }
+      if (subcategories.length > 0) {
+        let subcategoryOptions = () => {
+          let options = subcategories.map((subcategory, i) => {
+            // console.log(subcategory, i)
+            return (
+              <div key={i}>
+                <input 
+                  type="radio" name="filterKeySubcategory" 
+                  id={subcategory} value={subcategory}
+                />
+                <label htmlFor={subcategory} className="ml-1">{subcategory}</label>
+              </div>
+            )
+          })
+          return options
+        }
+        return (
+          <div className="filter">
+            <label className="filter-name">ประเภท{filterKeyCategory}</label>
+            <div>
+              <input 
+                type="radio" name="filterKeySubcategory" id="allsubcategories" 
+                value="all" defaultChecked
+              />
+              <label htmlFor="allsubcategories" className="ml-1">ทั้งหมด</label>
+            </div>
+            {subcategoryOptions()}
+          </div>
+        )
+      }
+    }
+  }
+
   filterPriceRange = () => {
     const { jsonData } = this.state;
     let priceRangeOptions = () => {
-      if (jsonData !== null) {
+      if (!!jsonData) {
         const { priceRange } = jsonData;
         let options = priceRange.map((range, i) => {
           return <option value={i} key={i}>{range}</option>
@@ -37,8 +156,8 @@ class App extends React.Component {
       }
     }    
     return (
-      <div>
-        <label htmlFor="filterPriceRange">ราคา</label>
+      <div className="filter">
+        <label className="filter-name" htmlFor="filterPriceRange">ราคา</label>
         <select className="form-control" id="filterPriceRange" defaultValue="">
           <option value="" disabled>กรุณาเลือก</option>
           <option value="all">ทั้งหมด</option>
@@ -51,7 +170,7 @@ class App extends React.Component {
   filterArea = () => {
     const { jsonData } = this.state;
     let provinceOptions = () => {
-      if (jsonData !== null) {
+      if (!!jsonData) {
         const { provinces } = jsonData;
         let options = provinces.map((province, i) => {
           return <option value={province} key={i}>{province}</option>
@@ -60,8 +179,8 @@ class App extends React.Component {
       }
     }    
     return (
-      <div>
-        <label htmlFor="filterArea">จังหวัด/ใกล้ฉัน</label>
+      <div className="filter">
+        <label className="filter-name" htmlFor="filterArea">จังหวัด/ใกล้ฉัน</label>
         <select className="form-control" id="filterArea">
           <option value="nearme" defaultValue>พื้นที่ใกล้ฉัน</option>
           <option value="all">พื้นที่ทั้งหมด</option>
@@ -72,19 +191,27 @@ class App extends React.Component {
   }
 
   merchants = () => {
-    if (this.state.jsonData !== null) {
+    if (!!this.state.jsonData) {
       const { merchantsFiltered } = this.state;
-      let merchants = merchantsFiltered.map((merchant, i) => {
+      if (merchantsFiltered.length > 0) {
+        let merchants = merchantsFiltered.map((merchant, i) => {
+          return (
+            <MerchantDetail
+              detail={merchant}
+              key={i}
+            />
+          )
+        })
+        return merchants
+      } else {
         return (
-          <MerchantDetail
-            detail={merchant}
-            key={i}
-          />
+          <div className="text-center">
+            <span>ขออภัย ไม่พบร้านค้าที่คุณต้องการ</span>
+          </div>
         )
-      })
-      return merchants
+      }
     } else {
-      console.log('Loading...')
+      console.log('Loading Data...')
       return (
         <p>Loading...</p>
       )
@@ -96,14 +223,15 @@ class App extends React.Component {
     return (
       <div>
         <nav className="navbar navbar-light bg-light">
-          <span className="navbar-brand mb-0 h1">Navbar</span>
+          <span className="navbar-brand mb-0 h1">คนละครึ่ง by YWC</span>
         </nav>
         <div className="container-fluid">
           <div className="row">
-            <div className="col-md-3">
-              <h2>Filter</h2>
+            <div className="filter-panel col-md-3">
+              {this.filterCategory()}
               {this.filterPriceRange()}
               {this.filterArea()}
+              {this.filterSubcategory()}
             </div>
             <div className="col-md-9">
               {this.merchants()}
--- src/MerchantDetail.js
+++ src/MerchantDetail.js
@@ -2,34 +2,69 @@ import React from 'react';
 import parse from 'html-react-parser';
 
 class MerchantDetail extends React.Component {
-    recommendedWord = (categoryName) => {
-        if (categoryName == 'ร้านอาหาร') {
-            return 'เมนูแนะนำ'
-        } else {
-            return 'สินค้าแนะนำ'
+    availableBadge = (isOpen) => {
+        if (isOpen == 'Y') {
+            return <span class="badge badge-success">เปิดอยู่</span>
+        } else if (isOpen == 'N') {
+            return <span class="badge badge-secondary">ปิดแล้ว</span>
         }
+    }
+    priceLevelStatus = (priceLevel) => {
+        if (!!priceLevel) {
+            return <span><span className="price-level"><i>{'฿'.repeat(priceLevel)}</i>{'฿'.repeat(4-priceLevel)}</span> |&nbsp;</span>
+        }
+    }
+    recommendedItemsBar = (categoryName, recommendedItems) => {
+        let recommendedWord = 'สินค้าแนะนำ'
+        if (categoryName == 'ร้านอาหาร') {
+            recommendedWord = 'เมนูแนะนำ'
+        }
+        if (!!recommendedItems && recommendedItems !== []) {
+            return <p><strong>{recommendedWord}</strong> {recommendedItems.join(', ')}</p>
+        }
+    }
+    facilitiesIcon = (facilities) => {
+        const materialIcons = {
+            'ที่จอดรถ': 'directions_car',
+            'รับจองล่วงหน้า': 'assignment',
+            'สามารถนำสัตว์เลี้ยงเข้าได้': 'pets',
+
+        }
+        let icons = facilities.map((facility, i) => {
+            return <span class="facility-icon material-icons" key={i}>{materialIcons[facility]}</span>
+        })
+        return icons
     }
     render() {
         let {
             shopNameTH, 
             highlightText,
             categoryName,
+            subcategoryName,
             coverImageId,
             recommendedItems,
             addressDistrictName,
             addressProvinceName,
+            isOpen,
+            priceLevel,
+            facilities
         } = this.props.detail
-        console.log(this.props.detail)
+        // console.log(this.props.detail)
         return (
             <div className="merchant">
                 <div className="img">
                     <img src={coverImageId}/>
                 </div>
                 <div className="body">
-                    <h2>{shopNameTH}</h2>
-                    <span>{categoryName} | {addressDistrictName} {addressProvinceName}</span>
+                    <h2>{shopNameTH} {this.availableBadge(isOpen)}</h2>
+                    <div className="details">
+                        <span>{subcategoryName} |&nbsp;</span>
+                        {this.priceLevelStatus(priceLevel)}
+                        <span>{addressDistrictName} {addressProvinceName}</span>
+                    </div>
                     <p>{parse(highlightText)}</p>
-                    <p><strong>{this.recommendedWord(categoryName)}</strong> {recommendedItems.join(', ')}</p>
+                    {this.recommendedItemsBar(categoryName, recommendedItems)}
+                    {this.facilitiesIcon(facilities)}
                 </div>
             </div>
         )
